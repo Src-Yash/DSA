eebd4b9a16655b9bf967506276be87d539c9be81
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-09-21 18:51:38 +0530

    day-5

--- Recursion/MazePath.cpp
+++ Recursion/MazePath.cpp
@@ -12,7 +12,19 @@ int maze(int sr,int sc,int er,int ec){
    return totalways;
    
 }
+
+void printPath(int sr,int sc,int er,int ec,string s){
+   if(sr>er || sc>ec) return;
+   if(sr==er && sc==ec){
+      cout<<s<<endl;
+      return;
+   }
+   printPath(sr,sc+1,er,ec,s+'R');
+   printPath(sr+1,sc,er,ec,s+'D');
+   
+}
 int main(){
-   cout<<maze(1,1,3,3);
+   cout<<maze(1,1,3,3)<<endl;
+   printPath(1,1,4,4,"");
    
 }

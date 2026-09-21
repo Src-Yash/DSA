449b40986b132d9bf832a09e1bbcdaa380d99a17
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-09-21 21:38:33 +0530

    more questions

--- Recursion/MazePath.cpp
+++ Recursion/MazePath.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 using namespace std;
 
+// using 4 parameters
+
 int maze(int sr,int sc,int er,int ec){
    if(sr>er || sc>ec) return 0;
    if(sr==er && sc==ec) return 1;
@@ -10,7 +12,17 @@ int maze(int sr,int sc,int er,int ec){
    int totalways = rightways + downways;
 
    return totalways;
-   
+}
+// using 2 parameters
+
+int maze2(int row,int col){
+   if(row<1 || col<1) return 0;
+   if(row==1 && col==1) return 1;
+   int rightways = maze2(row,col-1); // right
+   int downways = maze2(row-1,col); // down
+   int totalways = rightways + downways;
+
+   return totalways;
 }
 
 void printPath(int sr,int sc,int er,int ec,string s){
@@ -25,6 +37,7 @@ void printPath(int sr,int sc,int er,int ec,string s){
 }
 int main(){
    cout<<maze(1,1,3,3)<<endl;
-   printPath(1,1,4,4,"");
+   cout<<maze2(3,3)<<endl;
+   // printPath(1,1,4,4,"");
    
 }
---
+++ Recursion/PrintZigZag.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+void zigzag(int n){
+   if(n==0) return;
+   cout<<n;
+   zigzag(n-1);
+   cout<<n;
+   zigzag(n-1);
+   cout<<n;
+}
+
+int main(){
+   cout<<endl;
+   zigzag(3);
+   
+   return 0;  
+}
